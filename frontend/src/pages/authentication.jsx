d651d3c849056294b4c302e6f2ef48f57d74d66b
kumarshubhh <subhanshukumar290@gmail.com>
2025-03-01 20:18:02 +0530

    deployment Frontend

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -76,7 +76,7 @@ export default function Authentication() {
                     sm={4}
                     md={7}
                     sx={{
-                        backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+                        backgroundImage: 'url(https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg)',
                         backgroundRepeat: 'no-repeat',
                         backgroundColor: (t) =>
                             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
